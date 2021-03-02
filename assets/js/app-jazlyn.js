eb052ff795284b5c49d4c15722a3baa09605d4ef
JMazick <jazlynmm@outlook.com>
2021-03-01 22:31:19 -0600

    add jaz code

--- assets/js/app-jazlyn.js
+++ assets/js/app-jazlyn.js
@@ -1,5 +1,3 @@
-
-
  d3.csv('/data/Netflix_movies.csv').then(function(demoData) {
 
          // Step 1: Parse Data/Cast as numbers
@@ -158,61 +156,131 @@ d3.csv('/data/Total_Subscribers_by_country_2019_2020.csv').then(function(demoDat
 
 
 // bar graph
-// Define SVG area dimensions
-var svgWidth = 900;
-var svgHeight = 2000;
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 50, left: 70},
+    width = 650 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-// Define the chart's margins as an object
-var chartMargin = {
-  top: 20,
-  right: 20,
-  bottom: 20,
-  left: 20
-};
-
-// Define dimensions of the chart area
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
-
-// Select body, append SVG area to it, and set the dimensions
-var svg = d3
-  .select("body")
+// append the svg object to the body of the page
+var svg = d3.select(".chart")
   .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
-// to the margins set in the "chartMargin" object.
+// Parse the Data
+d3.csv("/data/rating_data_combined.csv").then(function(ratingData) {
+
+console.log(ratingData)
+
+ // List of subgroups = header of the csv files = soil condition here
+ var subgroups = ratingData.columns.slice(1)
+
+ console.log(subgroups)
+
+ // List of groups = species here = value of the first column called group -> I show them on the X axis
+ var groups = d3.map(ratingData, function(d){return(d.Rating)}).keys()
+
+ console.log(groups)
+
+  // Add X axis
+  var x = d3.scaleBand()
+      .domain(groups)
+      .range([0, width])
+      .padding([0.2])
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSizeOuter(0));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 3000])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // color palette = one color per subgroup
+  var color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(['#FA8072','#DC143C'])
+
+  //stack the data? --> stack per subgroup
+  var stackedData = d3.stack()
+    .keys(subgroups)
+    (ratingData)
+    // for (var i = 0; i < ratingData.length; i++){
+    // console.log(ratingData[i].Movie)
+    // }
+//   Show the bars
+  svg.append("g")
+    .selectAll("g")
+    // Enter in the stack data = loop key per key = group per group
+    .data(stackedData)
+    .enter().append("g")
+      .attr("fill", function(d) { return color(d.key); })
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(function(d) { return d; })
+      .enter().append("rect")
+        .attr("x", function(d) { return x(d.data.Rating); })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        .attr("width",x.bandwidth());
+
+// Create axes labels    
+svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left - 5)
+  .attr("x", 0 - (height / 2))
+  .attr("dy", "1em") // NOTE 'em' is 10px units and 'd'  is like a 'delta' up the y-axis in this case
+  .attr("class", "axisText")
+  .text("Number of Titles in Library");
+svg.append("text")
+  .attr("transform", `translate(${width / 2}, ${height + margin.top + 25})`)
+  .attr("class", "axisText")
+  .text("Rating");
+
+// Append group element
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Load data from csv
-d3.csv("/data/rating_data.csv").then(function(netflixMovieData) {
-
-  // Print the movie Data
-  console.log(netflixMovieData);
-
-  // Cast the hours value to a number for each piece of movie Data
-  netflixMovieData.forEach(function(netflixMovieData) {
-    netflixMovieData.rating = +netflixMovieData.rating;;
-  });
-
-  var barSpacing = 8; // desired space between each bar
-  var scaleY = 1; // 10x scale on rect height
-
-  // Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
-  var barWidth = (chartWidth - (barSpacing * (netflixMovieData.length - 1))) / netflixMovieData.length;
-
-  // Create code to build the bar chart using the movie Data.
-  chartGroup.selectAll(".bar")
-    .data(netflixMovieData)
-    .enter()
-    .append("rect")
-    .classed("bar", true)
-    .attr("width", d => barWidth) // equivalent to .attr("width", "91.11")
-    .attr("height", d => d.rating_cnt * scaleY)
-    .attr("x", (d, i) => i * (barWidth + barSpacing))
-    .attr("y", d => chartHeight - d.rating_cnt * scaleY);
-}).catch(function(error) {
-  console.log(error);
+// Step 1: Initialize Tooltip
+var toolTip = d3.tip() // method from the d3.tip library not native to d3 
+.attr("class", "tooltip")
+.offset([80, -60])
+.html(function(d) {
+  return (`<strong>${(d.Movie)}<strong><hr>${(d.TV_Show)}`);
+  console.log(d.Movie)
 });
+
+// Step 2: Create the tooltip in chartGroup.
+chartGroup.call(toolTip);
+
+// Step 3: Create "mouseover" event listener to display tooltip
+chartGroup.on("mouseover", function(d) {
+toolTip.show(d, this);
+})
+// Step 4: Create "mouseout" event listener to hide tooltip
+.on("mouseout", function(d) {
+  toolTip.hide(d);
+});
+
+// var tip = d3.tip()
+//   .attr('class', 'd3-tip')
+//   .offset([-10, 0])
+//   .html(function(d) {
+//     return "<strong>Count:</strong> <span style='color:red'>" + d.Movie + "</span> <strong>Count:</strong> <span style='color:red'>" + d.TV_Show + "</span>";
+//   })
+// var chartGroup = d3.select("body").append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// chartGroup.call(tip);
+
+    }).catch(function(error) {
+        console.log(error);
+       });
+      
