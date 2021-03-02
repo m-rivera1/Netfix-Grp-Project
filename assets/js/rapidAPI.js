2d2a4c14bba4c8ac4a2ec45b048e9563b10c71f4
inkosi-codes <kiwwanne@gmail.com>
2021-03-01 18:51:01 -0600

    Update rapidAPI.js

--- assets/js/rapidAPI.js
+++ assets/js/rapidAPI.js
@@ -3,7 +3,7 @@
 // fetch(`https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3A${country}&p=1&t=ns&st=adv`, {
 // 	"method": "GET",
 // 	"headers": {
-// 		"x-rapidapi-key": "37c9f27854msh06da57228f81f86p19df09jsnce75a4804c01",
+// 		"x-rapidapi-key": "",
 // 		"x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com",
 //         'content-type': 'application/json'
 // 	}
